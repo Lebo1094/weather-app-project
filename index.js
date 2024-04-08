9c605c7b48a1cd8993245b3d890532b99fe880cf
Lebo1094 <125014406+Lebo1094@users.noreply.github.com>
2024-04-08 10:07:34 +0200

    updated index.js

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-import axios from "axios";
+
 
 function search(event) {
   event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInputElement.value;
+  let searchInputElement = document.querySelector("#city-input");
+  let cityElement = document.querySelector("#city");
+  let city = searchInputElement.value;
 
   function showWeather(response) {
     let temperatureElement = document.querySelector("#temp-value");
     temperatureElement.innerHTML = Math.round(
       response.data.temperature.current
     );
+    cityElement.innerHTML = response.data.city;
   }
   let apiKey = "fd465o10bb292abbf346etb26bb03720";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityElement.innerHTML}
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}
 &key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(showWeather);
@@ -46,9 +47,9 @@ function formatDate(date) {
   return `${formattedDay} ${hours}:${minutes}`;
 }
 
-let searchForm = document.querySelector("#search-form");
+let searchForm = document.querySelector("#city-form");
 searchForm.addEventListener("submit", search);
 
-let currentDateELement = document.querySelector("#current-date");
+let currentDateELement = document.querySelector("#current-day");
 let currentDate = new Date();
 currentDateELement.innerHTML = formatDate(currentDate);
